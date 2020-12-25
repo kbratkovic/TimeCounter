d480cfff86b97cb751d48156a6e8da233ce09211
Kresimir Bratkovic <kbratkovic@gmail.com>
2020-12-26 00:35:42 +0100

    Resolve button start click

--- TimeCounter/Form1.cs
+++ TimeCounter/Form1.cs
@@ -5,7 +5,8 @@ namespace TimeCounter
 {
     public partial class Form1 : Form
     {
-        
+        private int totalSeconds;
+
         public Form1()
         {
             InitializeComponent();
@@ -16,6 +17,20 @@ namespace TimeCounter
                 inputMinutes.Items.Add(i.ToString());
                 inputSeconds.Items.Add(i.ToString());
             }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            labelOutput.Visible = true;
+
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+
+            int minutes = int.Parse(inputMinutes.SelectedItem.ToString());
+            int seconds = int.Parse(inputSeconds.SelectedItem.ToString());
+            totalSeconds = (minutes * 60) + seconds;
+
+            timer1.Enabled = true;
         }
 
     }
