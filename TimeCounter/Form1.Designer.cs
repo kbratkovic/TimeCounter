20466f3640028d619462d6ba4dad44ddf7d58bcd
Kresimir Bratkovic <kbratkovic@gmail.com>
2020-12-25 20:00:40 +0100

    Resolve form layout

--- TimeCounter/Form1.Designer.cs
+++ TimeCounter/Form1.Designer.cs
@@ -28,20 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.inputMinutes = new System.Windows.Forms.ComboBox();
+            this.inputSeconds = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStop.Location = new System.Drawing.Point(232, 238);
+            this.buttonStop.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(131, 64);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.Location = new System.Drawing.Point(85, 238);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(131, 64);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(81, 65);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(69, 20);
+            this.label.TabIndex = 4;
+            this.label.Text = "Minutes:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // inputMinutes
+            // 
+            this.inputMinutes.FormattingEnabled = true;
+            this.inputMinutes.Location = new System.Drawing.Point(85, 97);
+            this.inputMinutes.Name = "inputMinutes";
+            this.inputMinutes.Size = new System.Drawing.Size(121, 28);
+            this.inputMinutes.TabIndex = 5;
+            // 
+            // inputSeconds
+            // 
+            this.inputSeconds.FormattingEnabled = true;
+            this.inputSeconds.Location = new System.Drawing.Point(232, 97);
+            this.inputSeconds.Name = "inputSeconds";
+            this.inputSeconds.Size = new System.Drawing.Size(121, 28);
+            this.inputSeconds.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Seconds:";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Bold);
+            this.labelOutput.Location = new System.Drawing.Point(25, 65);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(407, 153);
+            this.labelOutput.TabIndex = 8;
+            this.labelOutput.Text = "00:00";
+            this.labelOutput.Visible = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(871, 582);
+            this.ClientSize = new System.Drawing.Size(454, 388);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputSeconds);
+            this.Controls.Add(this.inputMinutes);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonStop);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "TimeCounter";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox inputMinutes;
+        private System.Windows.Forms.ComboBox inputSeconds;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelOutput;
     }
 }
 
---
+++ TimeCounter/ReadMe.txt
@@ -0,0 +1,2 @@
+﻿Simple time counter
+
